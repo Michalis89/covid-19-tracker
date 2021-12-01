0d5a88ec96bcfc6f88792f6b7ebe89c684597775
Michalis Mouzakitis <mouzakitis.m89@gmail.com>
2021-12-01 04:04:05 +0200

    async api call for all countries dropdown

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
+import React, { useState, useEffect } from "react";
+import { FormControl, MenuItem, Select } from "@mui/material";
 import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+  useEffect(() => {
+    const getCountriesDate = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo,
+          }));
+          setCountries(countries);
+        });
+    };
+
+    getCountriesDate();
+  }, []);
+
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+    console.log(countryCode);
+    setCountry(countryCode);
+  };
+
   return (
     <div className="App">
-      <h1> Lets build a COVID 19 tracker! </h1>{" "}
+      <div className="app__header">
+        <h1>COVID-19 tracker!</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map((country) => (
+              // eslint-disable-next-line react/jsx-key
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
